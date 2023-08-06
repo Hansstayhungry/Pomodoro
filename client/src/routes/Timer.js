19f27fe465e38b2077457e1bc7f51db36f701d7b
Hans Wang <hansmikaa@live.cn>
2023-08-06 18:03:58 -0400

    added material UI, initalized components

---
+++ client/src/routes/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Timer = (props) => {
+
+  return (
+    <div className='timer'>
+      <h2>Timer</h2>
+    </div>
+  )
+}
+
+export default Timer;
---
+++ client/src/routes/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todo = (props) => {
+
+  return (
+    <div className='todo'>
+      <h2>To do</h2>
+    </div>
+  )
+}
+
+export default Todo;
