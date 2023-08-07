0616ce1a452e7339f322a68d2e0d5509a2d4c48b
Hans Wang <hansmikaa@live.cn>
2023-08-07 14:50:34 -0400

    added initial timer

--- client/src/routes/Timer.js
+++ client/src/routes/Timer.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Timer = (props) => {
+const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState(45*60);
+  const [isActive, setIsActive] = useState(false);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = (timeLeft - minutes * 60).toString().padStart(2, "0");
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && timeLeft > 0) {
+      interval = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, timeLeft]);
+
+  const handleStart = () => {
+    setIsActive(true);
+  };
+
+  const handleStop = () => {
+    setIsActive(false);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTimeLeft(45*60);
+  };
 
   return (
-    <div className='timer'>
-      <h2>Timer</h2>
+    <div className="timer">
+      <span>
+        {minutes}:{seconds}
+      </span>
+      <div className="buttons">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Timer;
