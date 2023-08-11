045cb9cacce47e4fcc75920f420050a067d044a2
Hans Wang <hansmikaa@live.cn>
2023-08-10 20:09:54 -0400

    fixed styling

--- client/src/components/AudioCard.js
+++ client/src/components/AudioCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardContent, Typography, CardActions, Button } from '@mui/material';
 
+import '../styles/AudioCard.scss'
+
 const AudioCard = (props) => {
   const { name, link, picture, handleAudioClick } = props
 
@@ -8,21 +10,25 @@ const AudioCard = (props) => {
     background: `url(${picture})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
+    minHeight: '300px'
   };
 
   return (
-    <Card variant="outlined" style={cardStyle} className="cardRoot">
-      <CardContent className="cardContent">
-        <Typography variant="h6" style={{ color: 'black' }}>
-          {name}
-        </Typography>
-      </CardContent>
-      <CardActions className="cardActions">
-        <Button onClick= {() => handleAudioClick(link)} color="primary">
-          Listen
-        </Button>
-      </CardActions>
-    </Card>
+    <div className='card-item'>
+      <Card variant="outlined" style={cardStyle} className="cardRoot">
+        <CardContent className="cardContent">
+          <Typography variant="h4">
+            <span style={{ color: '#F5F5DC' }}>{name}</span>
+          </Typography>
+        </CardContent>
+        <CardActions className="cardActions">
+          <Button onClick= {() => handleAudioClick(link)} style={{ backgroundColor: '#F5F5DC' }}>
+            <span>Listen</span>
+          </Button>
+        </CardActions>
+      </Card>      
+    </div>
+
   );
 };
 
